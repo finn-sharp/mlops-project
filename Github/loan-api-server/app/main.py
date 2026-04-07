536b108e1a55da46f803e8fee82e70b803ed4bc1
finn-sharp <penguin-klg@naver.com>
2026-04-07 10:03:48 +0900

    ch1 - BE 컨트롤러 구현 models에 저장된 ML Module을 사용하여 실제 컨트롤러에서 요구한 서버 실행 시, 모델 로드까지 구현

--- Github/loan-api-server/app/main.py
+++ Github/loan-api-server/app/main.py
@@ -1,8 +1,65 @@
-from fastapi import FastAPI
+# Java로 치면, 컨트롤러의 역할이라고 함
 
-app = FastAPI()
+from fastapi import FastAPI, HTTPException
+import logging
 
-# domain:  http://127.0.0.1:8000, Swagger UI :  http://127.0.0.1:8000/docs
-@app.get('/')
-def root():
-    return {'message' : "Hello"}
+from model import LoanModel
+from schemas import LoanResponse, LoanRequest
+
+logger = logging.getLogger(__name__)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """
+        Description : 모델을 서버 실행 시 한번만 로드
+    """
+    logger.info('대출 심사 모델을 로드합니다')
+    model = LoanModel()
+    try : 
+        model.load(model_dir='../models')
+        logger.info('모델 로드 성공')
+    except :
+        logger.warning('모델 로드 실패')
+        logger.warning('/predict 엔드포인트는 모델 로드 후 사용가능')
+    
+    app.state.model = model
+
+    yield 
+
+    logger.info('대출 심사 API를 종료합니다')
+
+app = FastAPI(
+    title = '대출 심사 예측 API',
+    description = 'ML 모델 기반 대출 승인 여부를 예측하는 API',
+    version = '1.0.0',
+    # 이렇게 인자로 넣어주면, app실행 시 lifespan 함수를 실행시켜 줌
+    lifespan = lifespan
+)
+
+# 서버가 살아있는지 체크하는 것 : 로드밸런서가 서버 생존 여부를 체크함***
+# 새로운 컴퓨터를 띄우는 역할 : ECS in AWS***
+@app.get('/health')
+async def health_check():
+    model = app.state.model
+    model_loaded = model.pipeline is not None
+    return {
+        "status" : "healthy" if model_loaded else "degraded",
+        "model_loaded" : model_loaded
+    }
+
+
+# 머신러닝 예측하는 API 로직( 정의한 schema로 통신 )
+@app.post("/predict", response_mode = LoanResponse)
+async def predict(request : LoanRequest):
+    model = app.state.model
+
+    try :    
+        model.predict(request.model_dump()) # model_dump : key를 english로 변환
+        LoanResponse(**result) # dict, 가변인자처리법
+
+    except RuntimeError as e:
+        raise HTTPException(status_code=503, detail=str(e))
+    except ValueError as e:
+        raise HTTPException(status_code=422, detail="입력값 처리오류")
+    except Exception as e:
+        raise HTTPException(status_code=500)
