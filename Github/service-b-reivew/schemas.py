26be6dc2c259b5d0fc3c13fb15731f72cf16e003
finn-sharp <penguin-klg@naver.com>
2026-04-07 11:01:55 +0900

    ch2 - 리뷰 관련 DTO 구현 리뷰 분석을 위한 DTO 구현으로 Response, Request에 필요한 schema를 구현하였음

---
+++ Github/service-b-reivew/schemas.py
@@ -0,0 +1,39 @@
+from pydantic import BaseModel, Field
+
+class AnalysisRequest(Basemodel):
+    """Review 데이터 입력 스키마"""
+    review_text:str = Field(
+        ...,
+        description="리뷰 원문",
+        examples=["배송이 너무 느려서 실망했어요. 상품 품질은 괜찮은데 포장이 엉망..."]
+    )
+    
+    model_config = {
+        "json_schema_extra" : {
+            "examples" : [
+                {
+                    "review_text": "배송이 너무 느려서 실망했어요. 상품 품질은 괜찮은데 포장이 엉망..."
+                }
+            ]
+        }
+    }
+
+class AnalysisResponse(BaseModel):
+    sentiment : str = Field(
+        ...,
+        description="감정분석 (True='긍정', False='부정')",
+    )
+    category : str = Field(
+        ...,
+        description="서비스유형 ('배송','상담','주문')",
+    )
+    summary : str = Field(
+        ...,
+        description="불만 사항에 대한 요약"
+    )
+    confidence : float = Field(
+        ...,
+        ge=0.0,
+        le=1.0,
+        description="?? 확률 (0.0 ~ 1.0)",
+    )
